03e0f45fd932382586f74bf6b10de24243115d08
高橋星乃介 <takahashihoshinosuke@HajimariHoshinosukeTakahashi.local>
2025-04-10 00:32:43 +0900

    practice 「フックの使用」の直前

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import lineIcon from './images/line-icon.jpg';
+import { useState } from 'react';
+
 function App() {
   return (
     <div className="App">
@@ -22,4 +25,113 @@ function App() {
   );
 }
 
-export default App;
+// function MyButton() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+function Image() {
+  return (
+    <img src={lineIcon} className="line-icon" />
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+}
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  )
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+// export default App;
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//       <AboutPage />
+//       <Image />
+//       <Profile />
+//     </div>
+//   );
+// }
+
+// export default function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen',
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+
+//   return (
+//     <ul>{listItems}</ul>
+//   )
+// }
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Couters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  )
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  )
+}
